639910d2e9928d7f1dd3fe9e3df6b360a3c69823
Ethorbit <hi1212003@gmail.com>
2020-08-25 23:11:11 -0700

    Add everything

--- gamemode/gamemodes/nzombies/gamemode/mapping/sv_mapsettings.lua
+++ gamemode/gamemodes/nzombies/gamemode/mapping/sv_mapsettings.lua
@@ -50,6 +50,7 @@ function nzMapping:LoadMapSettings(data)
 	if data.gamemodeentities then
 		nzMapping.Settings.gamemodeentities = data.gamemodeentities or nil
 	end
+
 	if data.specialroundtype then
 		nzMapping.Settings.specialroundtype = data.specialroundtype or "Hellhounds"
 	end
@@ -57,5 +58,11 @@ function nzMapping:LoadMapSettings(data)
 		nzMapping.Settings.bosstype = data.bosstype or "Panzer"
 	end
 
+	-- More compact and less messy:
+	for k,v in pairs(nzSounds.struct) do
+		nzMapping.Settings[v] = data[v] or {}
+	end
+
 	nzMapping:SendMapData()
+	nzSounds:RefreshSounds()
 end
