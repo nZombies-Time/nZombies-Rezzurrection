1d64fc3c72f1dc98ab7d2386e5632fbfce1ad222
Burso96 <ursob96@gmail.com>
2021-04-14 12:18:32 -0400

    Morphine Upgrade

---
+++ gamemode/lua/autorun/morphine_autorun.lua
@@ -0,0 +1,8 @@
+sound.Add(
+{
+    name = "Weapon_nzr_morphine.use",
+    channel = CHAN_WEAPON,
+    volume = 0.5,
+    soundlevel = 80,
+    sound = "weapons/morphine/revive_syrette.wav"
+})
---
+++ gamemode/lua/tfa/external/tfa_bo3_morphine.lua
@@ -0,0 +1,9 @@
+local path = "weapons/morphine/"
+local pref = "morphine"
+local hudcolor = Color(255, 255, 255, 255)
+
+TFA.AddWeaponSound(pref .. ".use", path .. "revive_syrette.wav")
+
+--if killicon and killicon.Add then
+--	killicon.Add("", "", hudcolor)
+--end
