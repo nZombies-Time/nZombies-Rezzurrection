3123daec05ec25a09cd13c11aa520373f758c12a
Ethorbit <hi1212003@gmail.com>
2021-07-14 15:33:31 -0700

    Remove NZ classic exploits

--- gamemode/gamemodes/nzombies/gamemode/misc/sv_consolecommands.lua
+++ gamemode/gamemodes/nzombies/gamemode/misc/sv_consolecommands.lua
@@ -1,9 +1,9 @@
 -- Actual Commands
 
 -- Quick reload for dedicated severs
-concommand.Add("nz_qr", function()
-	RunConsoleCommand("changelevel", game.GetMap())
-end)
+--concommand.Add("nz_qr", function()
+--	RunConsoleCommand("changelevel", game.GetMap())
+--end)
 
 concommand.Add( "nz_print_weps", function()
 	for k,v in pairs( weapons.GetList() ) do
@@ -16,11 +16,11 @@ concommand.Add("nz_door_id", function()
 	if IsValid( tr.Entity ) then print( tr.Entity:doorIndex() ) end
 end)
 
-concommand.Add("nz_test1", function()
-	nz.nzDoors.Functions.CreateMapDoorLink( 1236, "price=500,elec=0,link=1" )
-
-	timer.Simple(5, function() nz.nzDoors.Functions.RemoveMapDoorLink( 1236 ) end)
-end)
+--concommand.Add("nz_test1", function()
+--	nz.nzDoors.Functions.CreateMapDoorLink( 1236, "price=500,elec=0,link=1" )
+--
+--	timer.Simple(5, function() nz.nzDoors.Functions.RemoveMapDoorLink( 1236 ) end)
+--end)
 
 concommand.Add("nz_forceround", function(ply, cmd, args, argStr)
 	if !IsValid(ply) or ply:IsSuperAdmin() then
